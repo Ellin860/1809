965ac0ee8041d1a21dc97e6abe1eb2fa69e7ef35
Ellin860 <136501280+Ellin860@users.noreply.github.com>
2023-09-23 23:11:50 +0300

    Add files via upload

--- Tasc00/Task02/Program.cs
+++ Tasc00/Task02/Program.cs
@@ -1,25 +1,31 @@
 ﻿
-// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 
-Console.WriteLine("Введите  число а");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите  число в");
-int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
 
-if (a > b)
+
+if (number1 > number2 && number1 > number3)
 {
-    int max = a;
-    int min = b;
-    Console.WriteLine("Большее число = " + max);
-    Console.WriteLine("Mеньшее число = " + min);
+    Console.WriteLine("Максимальное из чисел = " + number1);
 }
-else
+
+else if (number2 >number1 && number2 > number3)
+
 {
-    int max = b;
-    int min = a;
-    Console.WriteLine("Большее число = " + max);
-    Console.WriteLine("Mеньшее число = " + min);
+    Console.WriteLine("Максимальное из чисел = " + number2);
 }
+
+else  if (number3 >number1 && number3 > number2)
+{
+    Console.WriteLine("Максимальное из чисел = " + number3);
+}
+
+
